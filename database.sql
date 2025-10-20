267c474bfea2df415fbc417fdcd6ce797949cdf1
Dima Ishchuk <siriusdima@gmail.com>
2025-10-20 23:02:19 +0300

    Create table modules

--- database.sql
+++ database.sql
@@ -1,1 +1,2 @@
 create table Courses (id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name varchar(255) not null, description text not null, created_at timestamp not null, updated_at timestamp not null);
+create table Modules (id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name varchar(255) not null, description text not null, created_at timestamp not null, updated_at timestamp not null);
