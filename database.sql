1f313b059aebfe8d642da58b756a895c189b2686
Dima Ishchuk <siriusdima@gmail.com>
2025-10-21 22:38:27 +0300

    Create table lessons

--- database.sql
+++ database.sql
@@ -1,2 +1,24 @@
-create table Courses (id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name varchar(255) not null, description text not null, created_at timestamp not null, updated_at timestamp not null);
-create table Modules (id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name varchar(255) not null, description text not null, created_at timestamp not null, updated_at timestamp not null);
+create table courses (
+			id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+			name varchar(255) not null, 
+			description text not null, 
+			created_at timestamp not null, 
+			updated_at timestamp not null
+			);
+create table modules (
+			id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+			name varchar(255) not null, 
+			description text not null, 
+			created_at timestamp not null, 
+			updated_at timestamp not null
+			);
+create table lessons (
+			id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+			name varchar(255) not null, 
+			description text not null, 
+			video_link varchar(255), 
+			lesson_number int not null, 
+			created_at timestamp not null, 
+			updated_at timestamp not null, 
+			course_id bigint references courses(id) not null
+			);
